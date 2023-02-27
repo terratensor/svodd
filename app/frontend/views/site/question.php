9fe5ea7672a7d2177d28b57fe195bd0ea53878cf
agusev <audetv@gmail.com>
2023-02-27 17:36:29 +0300

    Added collapsible question, changed pager

--- app/frontend/views/site/question.php
+++ app/frontend/views/site/question.php
@@ -18,14 +18,28 @@ $this->params['breadcrumbs'][] = $this->title;
 $commentsCount = $question->comments->getFacets()['group_type']['buckets'][0]['doc_count'] ?? 0;
 $linkedQuestionsCount = $question->linkedQuestions->getFacets()['group_type']['buckets'][0]['doc_count'] ?? 0;
 $page = Yii::$app->request->get()['page'] ?? 1;
+
+$show = $page <= 1;
+
+$js = <<<JS
+  const bsCollapse = new bootstrap.Collapse('#collapseQuestion', {
+  show: $show
+})
+JS;
+
+$this->registerJs($js);
+
 ?>
 <div class="site-index">
 
   <div class="row">
     <div class="col-md-12">
 
-        <?php if ($page && $page <= 1): ?>
+          <a class="btn btn-primary mb-3" data-bs-toggle="collapse" href="#collapseQuestion" role="button" aria-controls="collapseQuestion">
+            Просмотр вопроса
+          </a>
             <?php echo $this->render('question_card', ['question' => $question]); ?>
+        <?php if ($page && $page <= 1): ?>
             <?php if ($linkedQuestionsCount > 0) : ?>
                 <?= "<p><strong>Связанных вопросов: $linkedQuestionsCount </strong></p>"; ?>
                 <?php echo $this->render('question_linked', ['question' => $question]); ?>
@@ -38,6 +52,9 @@ $page = Yii::$app->request->get()['page'] ?? 1;
         echo LinkPager::widget(
             [
                 'pagination' => new Pagination(['totalCount' => $question->comments->getTotal()]),
+                'firstPageLabel' => true,
+                'lastPageLabel' => true,
+                'maxButtonCount' => 8,
             ]);
         ?>
     </div>
--- app/frontend/views/site/question_card.php
+++ app/frontend/views/site/question_card.php
@@ -8,15 +8,18 @@ use App\models\Question;
 use frontend\widgets\question\Card;
 
 ?>
-<?php foreach ($question->body as $hit): ?>
-<div class="card mb-4">
-    <div class="card-header d-flex justify-content-between">
-      <?= Card::widget(['hit' => $hit]); ?>
-    </div>
-    <div class="card-body">
-        <div class="card-text comment-text">
-            <?php echo Yii::$app->formatter->asRaw($hit->getData()['text']); ?>
+<div class="collapse" id="collapseQuestion">
+    <?php foreach ($question->body as $hit): ?>
+      <div class="card mb-4">
+        <div class="card-header d-flex justify-content-between">
+            <?= Card::widget(['hit' => $hit]); ?>
         </div>
-    </div>
+        <div class="card-body">
+          <div class="card-text comment-text">
+              <?php echo Yii::$app->formatter->asRaw($hit->getData()['text']); ?>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?>
 </div>
-<?php endforeach; ?>
+
