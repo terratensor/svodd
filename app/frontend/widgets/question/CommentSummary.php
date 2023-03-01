4e284c7d044019ee15313e8d9de37de358dc25cd
Aleksey Gusev <audetv@gmail.com>
2023-03-01 22:09:27 +0300

    Fixed CommentSummary

--- app/frontend/widgets/question/CommentSummary.php
+++ app/frontend/widgets/question/CommentSummary.php
@@ -2,31 +2,28 @@
 
 namespace frontend\widgets\question;
 
-use Manticoresearch\ResultSet;use yii\bootstrap5\Widget;use yii\helpers\Html;
+use yii\bootstrap5\Widget;
+use yii\data\Pagination;
+use yii\helpers\Html;
 
 class CommentSummary extends Widget
 {
-    public ?int $pageSize = null;
-    public int $page = 1;
-    public int $summary = 0;
-
-    public function init()
-    {
-        if ($this->pageSize === null) {
-            $this->pageSize = \Yii::$app->params['questions']['pageSize'];
-        }
-    }
+    public Pagination $pagination;
 
     public function renderSummary(): string
     {
-        $start = ($this->page * $this->pageSize - $this->pageSize) + 1;
-        $end = $this->page * $this->pageSize;
+        $totalCount = $this->pagination->totalCount;
+        $start = (($this->pagination->getPage() + 1) * $this->pagination->pageSize - $this->pagination->pageSize) + 1;
+        $end = ($this->pagination->getPage() + 1) * $this->pagination->pageSize;
+        if ($end > $totalCount) {
+            $end = $totalCount;
+        }
 
         $string = \Yii::t(
             'app',
             'Показано {start} – {end} из {n, plural, =0{Нет комментариев} few{# комментария} many{# комментариев} other{# комментариев}}',
             [
-                'n' => $this->summary,
+                'n' => $totalCount,
                 'start' => $start,
                 'end' => $end
             ]
