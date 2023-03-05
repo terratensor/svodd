0c591578fb15fc7daea6c94122e33c0f9d322aad
Aleksey Gusev <audetv@gmail.com>
2023-03-05 23:04:25 +0300

    Started Bookmark controller

---
+++ app/frontend/controllers/BookmarkController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace frontend\controllers;
+
+use App\Bookmark\Http\Action\V1\Bookmark\Comment\CreateAction;
+use yii\web\Controller;
+
+class BookmarkController extends Controller
+{
+    public function actions(): array
+    {
+        return [
+            'create' => [
+                'class' => CreateAction::class,
+            ],
+        ];
+    }
+}
