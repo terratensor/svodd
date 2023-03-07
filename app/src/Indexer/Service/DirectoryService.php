3813b23b962e8223a11b34295b224e26639e9f20
Aleksey Gusev <audetv@gmail.com>
2023-03-07 21:26:10 +0300

    Added DirectoryService.php

--- app/src/Indexer/Service/DirectoryService.php
+++ app/src/Indexer/Service/DirectoryService.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace App\Indexer\Service;
 
+use Yii;
+
 class DirectoryService
 {
     public function readDir(): array
     {
         $arrFiles = array();
 
-        $handle = opendir(__DIR__ . '/../../../data/test');
+        $handle = opendir(Yii::$app->params['questionIndexFolder']);
         if ($handle) {
             while (false !== ($entry = readdir($handle))) {
                 if ($entry != "." && $entry != ".." && $entry != ".gitignore") {
