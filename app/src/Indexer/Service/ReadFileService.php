0c2c9cc64f4187758974987dd0ae01f5f7fddbd9
Aleksey Gusev <audetv@gmail.com>
2023-03-12 22:33:32 +0300

    Added normalize url

--- app/src/Indexer/Service/ReadFileService.php
+++ app/src/Indexer/Service/ReadFileService.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Indexer\Service;
 
 use Yii;
+use yii\helpers\BaseFileHelper;
 
 class ReadFileService
 {
     public function readFile(string $file): string
     {
-        if (!($file = file_get_contents(Yii::$app->params['questionIndexFolder'] . $file))) {
+        $path = BaseFileHelper::normalizePath(Yii::$app->params['questionIndexFolder'] . '/' . $file);
+
+        if (!($file = file_get_contents($path))) {
             throw new \DomainException("Failure to read file: $file");
         }
         return $file;
