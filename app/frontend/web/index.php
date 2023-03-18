457003280a1f085ddbed31e62768a9d6e7537737
agusev <audetv@gmail.com>
2023-03-18 10:46:24 +0300

    Added index to the git cache

---
+++ app/frontend/web/index.php
@@ -0,0 +1,25 @@
+<?php
+
+if ((getenv('APP_ENV') !== 'prod')) {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+}
+
+
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../../common/config/bootstrap.php';
+require __DIR__ . '/../config/bootstrap.php';
+
+if ((getenv('APP_ENV') === 'prod')) {
+    \Sentry\init(['dsn' => trim(file_get_contents(getenv('SENTRY_DSN_FILE')))]);
+}
+
+$config = yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/../../common/config/main.php',
+    require __DIR__ . '/../../common/config/main-local.php',
+    require __DIR__ . '/../config/main.php',
+    require __DIR__ . '/../config/main-local.php'
+);
+
+(new yii\web\Application($config))->run();
