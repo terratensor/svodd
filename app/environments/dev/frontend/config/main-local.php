c8c4d67d230322c0c895195b30422c2050c18894
agusev <audetv@gmail.com>
2023-03-22 13:00:43 +0300

    Fixed debug panel

--- app/environments/dev/frontend/config/main-local.php
+++ app/environments/dev/frontend/config/main-local.php
@@ -9,11 +9,12 @@ $config = [
     ],
 ];
 
-if (!YII_ENV_TEST) {
+if (getenv('APP_ENV') != 'prod') {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => \yii\debug\Module::class,
+        'allowedIPs' => ['*'],
     ];
 
     $config['bootstrap'][] = 'gii';
