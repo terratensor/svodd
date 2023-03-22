cc1bb35a8c0d9fd60a0c9bb68505372d320de736
agusev <audetv@gmail.com>
2023-03-22 11:11:57 +0300

    Extended Feedback form

--- app/src/Feedback/Form/SendMessage/FeedbackForm.php
+++ app/src/Feedback/Form/SendMessage/FeedbackForm.php
@@ -2,11 +2,20 @@
 
 namespace App\Feedback\Form\SendMessage;
 
+use App\Feedback\Entity\Feedback\Feedback;
 use yii\base\Model;
 
 class FeedbackForm extends Model
 {
     public string $text = '';
+
+    public function __construct(?Feedback $feedback = null, $config = [])
+    {
+        if ($feedback) {
+            $this->text = $feedback->text;
+        }
+        parent::__construct($config);
+    }
 
     public function rules(): array
     {
