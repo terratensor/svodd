01b0fc0839ce077279d3386b2e64b0351eeb593c
agusev <audetv@gmail.com>
2023-03-23 11:53:16 +0300

    Added factory for creating topic related question from db

--- app/src/Indexer/Model/RelatedQuestion.php
+++ app/src/Indexer/Model/RelatedQuestion.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Indexer\Model;
 
+use App\Question\Entity\Question\Question as DbQuestion;
 use DateTimeImmutable;
 use Exception;
 use stdClass;
@@ -27,6 +28,24 @@ class RelatedQuestion
         $this->datetime = new DateTimeImmutable($data->datetime);
         $this->parent_id = (int)$data->parent_id;
         $this->type = (int) $data->type;
+    }
+
+    /**
+     * @throws Exception
+     * Создает объект Topic RelatedQuestion из данных DB
+     */
+    public static function createFromDB(DbQuestion $dbQuestion): self
+    {
+        $data = new stdClass();
+
+        $data->username = $dbQuestion->username;
+        $data->role = $dbQuestion->user_role;
+        $data->text = $dbQuestion->text;
+        $data->datetime = $dbQuestion->datetime->format('H:i d.m.Y');;
+        $data->parent_id = $dbQuestion->parent_data_id;
+        $data->type = $dbQuestion->getType();
+
+        return new RelatedQuestion($data);
     }
 
     public function getSource(): array
