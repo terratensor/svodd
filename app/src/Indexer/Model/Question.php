5b066397aa5abf7c379a25e5a6afb40980f9d6f2
agusev <audetv@gmail.com>
2023-03-23 11:53:07 +0300

    Added factory for creating topic question from db

--- app/src/Indexer/Model/Question.php
+++ app/src/Indexer/Model/Question.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Indexer\Model;
 
+use App\Question\Entity\Question\Question as DbQuestion;
 use DateTimeImmutable;
 use Exception;
 use stdClass;
@@ -27,6 +28,24 @@ class Question
         $this->datetime = new DateTimeImmutable($data->datetime);
         $this->data_id = (int)$data->data_id;
         $this->type = (int) $data->type;
+    }
+
+    /**
+     * @throws Exception
+     * Создает объект Topic Question из данных DB
+     */
+    public static function createFromDB(DbQuestion $dbQuestion): self
+    {
+        $data = new stdClass();
+
+        $data->username = $dbQuestion->username;
+        $data->role = $dbQuestion->user_role;
+        $data->text = $dbQuestion->text;
+        $data->datetime = $dbQuestion->datetime->format('H:i d.m.Y');
+        $data->data_id = $dbQuestion->data_id;
+        $data->type = $dbQuestion->getType();
+
+        return new Question($data);
     }
 
     public function getSource(): array
