78d2d790ee38a33d68f42dfbf4d2eed2d82165a1
agusev <audetv@gmail.com>
2023-03-23 15:43:49 +0300

    Moved registration link to the login page

--- app/frontend/views/auth/auth/login.php
+++ app/frontend/views/auth/auth/login.php
@@ -2,6 +2,7 @@
 
 /** @var yii\web\View $this */
 /** @var yii\bootstrap5\ActiveForm $form */
+
 /** @var LoginForm $model */
 
 use yii\bootstrap5\Html;
@@ -12,31 +13,32 @@ $this->title = 'Вход';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h4><?= Html::encode($this->title) ?></h4>
+  <h4><?= Html::encode($this->title) ?></h4>
 
-    <p>Пожалуйста, заполните форму ниже для входа:</p>
+  <p>Введите email и пароль для входа <br>
+    или <?= Html::a('зарегистрируйтесь', ['auth/join/request']); ?></p>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+  <div class="row">
+    <div class="col-lg-5">
+        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-                <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+        <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+        <?= $form->field($model, 'password')->passwordInput() ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+        <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-                <div class="my-1 mx-0" style="color:#999;">
-                  Если вы забыли пароль, вы можете его <?= Html::a('сбросить', ['auth/reset/password-request']) ?>
-                    <br>
-                    Нужно подтверждение email? <?= Html::a('Отправить повторно', ['auth/join/resend']) ?>
-                </div>
+      <div class="my-1 mx-0" style="color:#999;">
+        Если вы забыли пароль, вы можете его <?= Html::a('сбросить', ['auth/reset/password-request']) ?>
+        <br>
+        Нужно подтверждение email? <?= Html::a('Отправить повторно', ['auth/join/resend']) ?>
+      </div>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Вход', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
+      <div class="form-group">
+          <?= Html::submitButton('Вход', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+      </div>
 
-            <?php ActiveForm::end(); ?>
-        </div>
+        <?php ActiveForm::end(); ?>
     </div>
+  </div>
 </div>
