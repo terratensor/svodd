d9b51717557c9cf50f1c403d3467d21a65a6e3b1
agusev <audetv@gmail.com>
2023-03-23 14:46:20 +0300

    improved search filter

--- app/src/helpers/SearchHelper.php
+++ app/src/helpers/SearchHelper.php
@@ -4,9 +4,13 @@ namespace App\helpers;
 
 class SearchHelper
 {
-    public static function filter(string $queryString): string
+    public static function filter(string $queryString, $disabled = false): string
     {
-        $queryString = trim(filter_var($queryString, FILTER_SANITIZE_ADD_SLASHES), " \!\\-\n\r\t\v\x00");
-        return preg_replace('~(.)\1+~i', '\\2', $queryString);
+        if (!$disabled) {
+            $queryString = trim(filter_var($queryString, FILTER_SANITIZE_ADD_SLASHES), " \!\\-\n\r\t\v\x00");
+            $queryString = preg_replace('~(.)\1+~i', '\\2', $queryString);
+            $queryString = preg_replace('~[!-]~', '\\2', $queryString);
+        }
+        return $queryString;
     }
 }
