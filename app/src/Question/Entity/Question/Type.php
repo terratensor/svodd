d713fb423f5ad145fdfa2da5d7df7a83ef16d2c6
agusev <audetv@gmail.com>
2023-03-23 11:50:29 +0300

    Added entity question type const

---
+++ app/src/Question/Entity/Question/Type.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Question\Entity\Question;
+
+class Type
+{
+    public const QUESTION = 1;
+    public const RELATED_QUESTION = 2;
+    public const QUESTION_COMMENT = 3;
+    public const UNDEFINED = 0;
+}
