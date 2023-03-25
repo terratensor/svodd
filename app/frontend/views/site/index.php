9814a9bf3a2317c9994c6b1ca2078bae3456a560
agusev <audetv@gmail.com>
2023-03-25 16:16:08 +0300

    Changed search form, added fixed

--- app/frontend/views/site/index.php
+++ app/frontend/views/site/index.php
@@ -26,124 +26,160 @@ $this->title = 'ФКТ поиск';
 $this->title = 'Поиск по архиву вопросов и комментариев сайта ФКТ:';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-index">
-  <div class="w-100">
-      <?php if (!$results): ?>
-      <?php endif; ?>
+  <div class="site-index">
+    <div class="search-block">
+      <div class="container bg-white">
+          <?php if (!$results): ?>
+          <?php endif; ?>
 
-      <?php $form = ActiveForm::begin(
-          [
-              'method' => 'GET',
-              'action' => ['site/index'],
-              'options' => ['class' => 'mt-3'],
-          ]
-      ); ?>
-    <div class="d-flex align-items-center">
-        <?= $form->field($model, 'query', [
-            'inputTemplate' => '<div class="input-group mb-3">
+          <?php $form = ActiveForm::begin(
+              [
+                  'method' => 'GET',
+                  'action' => ['site/index'],
+                  'options' => ['class' => 'pt-3'],
+              ]
+          ); ?>
+        <div class="d-flex align-items-center">
+            <?= $form->field($model, 'query', [
+                'inputTemplate' => '<div class="input-group mb-3">
           {input}
           <button class="btn btn-outline-primary" type="submit" id="button-addon2">Поиск</button></div>',
-            'options' => [
-                'class' => 'w-100', 'role' => 'search'
-            ]
-        ])->textInput(
-            [
-                'class' => 'form-control form-control-lg',
-                'placeholder' => "Поиск"
-            ]
-        )->label(false); ?>
-    </div>
-      <?php ActiveForm::end(); ?>
-  </div>
-
-    <?php if ($results): ?>
-    <?php
-    // Property totalCount пусто пока не вызваны данные модели getModels(),
-    // сначала получаем массив моделей, потом получаем общее их количество
-    /** @var Comment[] $comments */
-    $comments = $results->getModels();
-    $pagination = new Pagination(
-        [
-            'totalCount' => $results->getTotalCount(),
-            'defaultPageSize' => Yii::$app->params['questions']['pageSize'],
-        ]
-    );
-    ?>
-  <div class="row">
-    <div class="col-md-12">
-      <p><strong>Всего результатов: <?= $results->getTotalCount(); ?></strong></p>
-      <div class="row">
-        <div class="col-md-8 d-flex align-items-center">
-            <?= CommentSummary::widget(['pagination' => $pagination]); ?>
+                'options' => [
+                    'class' => 'w-100', 'role' => 'search'
+                ]
+            ])->textInput(
+                [
+                    'class' => 'form-control form-control-lg',
+                    'placeholder' => "Поиск"
+                ]
+            )->label(false); ?>
         </div>
-        <div class="col-md-4">
-          <div class="d-flex align-items-start ">
-            <label aria-label="Сортировка" for="input-sort"></label>
-            <select id="input-sort" class="form-select mb-3" onchange="location = this.value;">
-                <?php
-                $values = [
-                    '' => 'Сортировка по умолчанию',
-                    'datetime' => 'Сначала старые комментарии',
-                    '-datetime' => 'Сначала новые комментарии',
-                ];
-                $current = Yii::$app->request->get('sort');
-                ?>
-                <?php foreach ($values as $value => $label): ?>
-                  <option value="<?= Html::encode(Url::current(['sort' => $value ?: null])) ?>"
-                          <?php if ($current == $value): ?>selected="selected"<?php endif; ?>><?= $label ?></option>
-                <?php endforeach; ?>
-            </select>
-          </div>
-        </div>
+          <?php ActiveForm::end(); ?>
       </div>
-        <?php foreach ($comments as $comment): ?>
-          <div class="card mb-4">
-            <div class="card-header d-flex justify-content-between">
-              <div><?= DateHelper::showDateFromTimestamp($comment->datetime); ?>, <?= $comment->username; ?></div>
-              <div><?= "#" . $comment->data_id; ?></div>
+    </div>
+    <div class="container">
+        <?php if ($results): ?>
+        <?php
+        // Property totalCount пусто пока не вызваны данные модели getModels(),
+        // сначала получаем массив моделей, потом получаем общее их количество
+        /** @var Comment[] $comments */
+        $comments = $results->getModels();
+        $pagination = new Pagination(
+            [
+                'totalCount' => $results->getTotalCount(),
+                'defaultPageSize' => Yii::$app->params['questions']['pageSize'],
+            ]
+        );
+        ?>
+      <div class="row">
+        <div class="col-md-12">
+          <p><strong>Всего результатов: <?= $results->getTotalCount(); ?></strong></p>
+          <div class="row">
+            <div class="col-md-8 d-flex align-items-center">
+                <?= CommentSummary::widget(['pagination' => $pagination]); ?>
             </div>
-            <div class="card-body">
-                <?php foreach ($comment->highlight as $field => $snippets): ?>
-                  <div class="card-text comment-text">
-                      <?php foreach ($snippets as $snippet): ?>
-                          <?php echo Yii::$app->formatter->asRaw(htmlspecialchars_decode($snippet)); ?>
-                      <?php endforeach; ?>
-                  </div>
-                <?php endforeach; ?>
+            <div class="col-md-4">
+              <div class="d-flex align-items-start ">
+                <label aria-label="Сортировка" for="input-sort"></label>
+                <select id="input-sort" class="form-select mb-3" onchange="location = this.value;">
+                    <?php
+                    $values = [
+                        '' => 'Сортировка по умолчанию',
+                        'datetime' => 'Сначала старые комментарии',
+                        '-datetime' => 'Сначала новые комментарии',
+                    ];
+                    $current = Yii::$app->request->get('sort');
+                    ?>
+                    <?php foreach ($values as $value => $label): ?>
+                      <option value="<?= Html::encode(Url::current(['sort' => $value ?: null])) ?>"
+                              <?php if ($current == $value): ?>selected="selected"<?php endif; ?>><?= $label ?></option>
+                    <?php endforeach; ?>
+                </select>
+              </div>
             </div>
-            <div class="card-footer d-flex justify-content-between">
-                <?= FollowQuestion::widget(['comment' => $comment, 'pagination' => $pagination]); ?>
-                <?php
-                $id = ($comment->type === 1) ? $comment->data_id : $comment->parent_id;
-                $link = "https://фкт-алтай.рф/qa/question/view-" . $id;
-                ?>
-                <?= Html::a(
-                    $link,
-                    $link . "#:~:text=" . Yii::$app->formatter->asDatetime($comment->datetime, 'php:H:i d.m.Y'),
-                    ['target' => '_blank']
+          </div>
+            <?php foreach ($comments as $comment): ?>
+              <div class="card mb-4">
+                <div class="card-header d-flex justify-content-between">
+                  <div><?= DateHelper::showDateFromTimestamp($comment->datetime); ?>, <?= $comment->username; ?></div>
+                  <div><?= "#" . $comment->data_id; ?></div>
+                </div>
+                <div class="card-body">
+                    <?php foreach ($comment->highlight as $field => $snippets): ?>
+                      <div class="card-text comment-text">
+                          <?php foreach ($snippets as $snippet): ?>
+                              <?php echo Yii::$app->formatter->asRaw(htmlspecialchars_decode($snippet)); ?>
+                          <?php endforeach; ?>
+                      </div>
+                    <?php endforeach; ?>
+                </div>
+                <div class="card-footer d-flex justify-content-between">
+                    <?= FollowQuestion::widget(['comment' => $comment, 'pagination' => $pagination]); ?>
+                    <?php
+                    $id = ($comment->type === 1) ? $comment->data_id : $comment->parent_id;
+                    $link = "https://фкт-алтай.рф/qa/question/view-" . $id;
+                    ?>
+                    <?= Html::a(
+                        $link,
+                        $link . "#:~:text=" . Yii::$app->formatter->asDatetime($comment->datetime, 'php:H:i d.m.Y'),
+                        ['target' => '_blank']
+                    ); ?>
+                </div>
+              </div>
+            <?php endforeach; ?>
+
+          <div class="container container-pagination">
+            <div class="detachable fixed-bottom">
+                <?php echo LinkPager::widget(
+                    [
+                        'pagination' => $pagination,
+                        'firstPageLabel' => true,
+                        'lastPageLabel' => true,
+                        'maxButtonCount' => 5,
+                        'options' => [
+                            'class' => 'd-flex justify-content-center'
+                        ],
+                        'listOptions' => ['class' => 'pagination mb-0']
+                    ]
                 ); ?>
             </div>
           </div>
-        <?php endforeach; ?>
 
-      <div class="container container-pagination">
-        <div class="detachable fixed-bottom">
-            <?php echo LinkPager::widget(
-                [
-                    'pagination' => $pagination,
-                    'firstPageLabel' => true,
-                    'lastPageLabel' => true,
-                    'maxButtonCount' => 5,
-                    'options' => [
-                        'class' => 'd-flex justify-content-center'
-                    ],
-                    'listOptions' => ['class' => 'pagination mb-0']
-                ]
-            ); ?>
         </div>
       </div>
-
     </div>
+      <?php endif; ?>
   </div>
-</div>
-<?php endif; ?>
+
+
+<?php $js = <<<JS
+  let menu = $(".search-block");
+var menuOffsetTop = menu.offset().top;
+var menuHeight = menu.outerHeight();
+var menuParent = menu.parent();
+var menuParentPaddingTop = parseFloat(menuParent.css("padding-top"));
+
+console.log("menuHeight: ", menuHeight)
+ 
+checkWidth();
+ 
+function checkWidth() {
+    if (menu.length !== 0) {
+      $(window).scroll(onScroll);
+    }
+}
+ 
+function onScroll() {
+  console.log("scrollTop", $(window).scrollTop())
+  console.log("menuOffsetTop", menuOffsetTop)
+  if ($(window).scrollTop() > menuOffsetTop) {
+    menu.addClass("shadow");
+    menuParent.css({ "padding-top": menuParentPaddingTop - menuHeight });
+  } else {
+    menu.removeClass("shadow");
+    menuParent.css({ "padding-top": menuParentPaddingTop });
+  }
+}
+JS;
+
+$this->registerJs($js);
