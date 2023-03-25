65cec45dd49e04612542b614820aec94a2efe7da
agusev <audetv@gmail.com>
2023-03-25 16:14:52 +0300

    Added header layout section

---
+++ app/frontend/views/layouts/header.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+;
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+
+$menuItems = [
+    ['label' => 'Поиск', 'url' => ['/site/index']],
+    ['label' => 'СВОДД', 'url' => ['/svodd/index']],
+    [
+        'label' => 'Обсуждение',
+        'url' => array_merge(['/svodd/view'], Yii::$app->session->get('svodd') ?? []),
+        // добавляем параметры предыдущего запроса к ссылке на обсуждение, чтобы восстановить сортировку и страницу
+    ],
+    ['label' => 'Вопросы', 'url' => ['/question/index']],
+];
+
+?>
+<header>
+    <?php NavBar::begin(
+        [
+            'collapseOptions' => false,
+            'options' => [
+                'class' => 'navbar navbar-expand navbar-dark bg-dark fixed-top',
+                'style' => "background-color: #e3f2fd;"
+            ],
+        ]);
+
+    echo Nav::widget(
+        [
+            'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
+            'items' => $menuItems,
+        ]);
+    NavBar::end();
+    ?>
+</header>
