4ce795cf0db5db5c5c99d83002681dab9ae7eaab
agusev <audetv@gmail.com>
2023-03-31 21:32:16 +0300

    Изменена функция для показа версии приложения

--- app/src/helpers/VersionHelper.php
+++ app/src/helpers/VersionHelper.php
@@ -9,35 +9,12 @@ use yii\helpers\Html;
 class VersionHelper
 {
     /**
-     * Возвращает версию приложения из репозитория github
+     * Возвращает версию приложения
      * @return string
      */
     public static function version(): string
     {
-        $cache = \Yii::$app->cache;
-
-        return $cache->getOrSet('version', function () {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                'Accept: application/vnd.github+json',
-                'X-GitHub-Api-Version: 2022-11-28',
-                'User-Agent: fct-search-app'
-            ]);
-            curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/repos/audetv/fct-search/releases/latest');
-            $result = curl_exec($ch);
-            curl_close($ch);
-
-            $json = json_decode($result, true);
-
-            $tag_name = $json['tag_name'] ?? '';
-            $html_url = $json['html_url'] ?? '';
-
-            if ($tag_name && $html_url) {
-                return Html::a($json['tag_name'], $json['html_url'], ['target' => '_blank']);
-            }
-
-            return '';
-        });
+        $version = 'v0.2.2';
+        return Html::a($version, 'https://github.com/audetv/fct-search/releases/latest', ['target' => '_blank']);
     }
 }
