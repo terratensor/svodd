7ad4b07a3e0524f6d74fce0457fa3b7916d7dc42
agusev <audetv@gmail.com>
2023-04-02 21:12:09 +0300

    added summary

--- app/frontend/views/svodd/index.php
+++ app/frontend/views/svodd/index.php
@@ -10,11 +10,9 @@ use App\Question\Entity\Statistic\QuestionStats;
 use App\Svodd\Entity\Chart\Data;
 use frontend\widgets\ChartJs\Chart;
 use frontend\widgets\question\SvoddListWidget;
+use yii\helpers\Url;
 use yii\web\JsExpression;
 use yii\web\View;
-
-//$this->title = 'Обратная хронология обсуждения СВОДД';
-//$this->params['breadcrumbs'][] = $this->title;
 
 /** @var Data[] $data */
 $data = Data::find()->orderBy('topic_number DESC')->all();
@@ -25,42 +23,56 @@ $datasetSvodd = [];
 $datasetFct = [];
 $dataLabelSvodd = [];
 $dataLabelFct = [];
+$current = 0;
+$summary = 0;
+$summarySvodd = 0;
+$summaryFct = 0;
 
-foreach ($data as $item) {
+foreach ($data as $key => $item) {
 
     $labels[] = sprintf("%02d", $item->topic_number) . '. ' . $item->title . ' ' . $item->questionStats->url;
     $labelLinks[] = $item->questionStats->url;
     $dataLabelSvodd[] = $svodd = $item->comments_count;
     $dataLabelFct[] = $fct = $item->end_comment_data_id - $item->start_comment_data_id - $item->comments_count;
+    if ($key === 0) {
+        $current = round($svodd / ($svodd + $fct) * 100, 2);
+    }
     $datasetSvodd[] = round($svodd / ($svodd + $fct) * 100, 2);
     $datasetFct[] = round($fct / ($svodd + $fct) * 100, 2);
+
+    $summarySvodd = $summarySvodd + $svodd;
+    $summaryFct = $summaryFct + $fct;
 }
+
+$summary = round($summarySvodd / ($summarySvodd + $summaryFct) * 100, 2);
 
 $callback = <<<JS
   function (value, index, ticks, test) {   
       if ($(window).width() > 768) {
         return this.getLabelForValue(value);          
       } else {
-        // console.log(ticks.length - value)
         return ticks.length - value
       }
   }
 JS;
 
 ?>
-<div class="py-5">
-  <div class="row">
-    <div class="col-xl-8">
-      <h1 class="bd-title mt-0">СВОДД</h1>
-      <p class="bd-lead">Обратная хронология обсуждения событий, статистика комментариев в теме СВОДД и в остальных темах ФКТ.</p>
-      <div class="d-flex flex-column flex-md-row gap-3">
-        <a href="<?= \yii\helpers\Url::to(['svodd/view'])?>" class="btn btn-lg bd-btn-lg btn-bd-primary d-flex align-items-center justify-content-center btn-svodd">
-          Всё обсуждение СВОДД одним потоком
-        </a>
+  <div class="py-5">
+    <div class="row">
+      <div class="col-xl-8">
+        <h1 class="bd-title mt-0">СВОДД</h1>
+        <p class="bd-lead">Обратная хронология обсуждения событий, статистика комментариев в теме СВОДД и в остальных
+          темах ФКТ.</p>
+        <div class="d-flex flex-column flex-md-row gap-3">
+          <a href="<?= Url::to(['svodd/view']) ?>"
+             class="btn btn-lg bd-btn-lg btn-bd-primary d-flex align-items-center justify-content-center btn-svodd">
+            Всё одним потоком
+          </a>
+        </div>
       </div>
     </div>
   </div>
-</div>
+  <h6>Текущая <?= $current; ?>% Всего <?= $summary; ?>%</h6>
 <?php echo Chart::widget(
     [
         'id' => 'svoddDiagram',
@@ -87,9 +99,17 @@ JS;
         ],
         'plugins' => '[ChartDataLabels]',
         'options' => [
+            'layout' => [
+                'padding' => [
+                    'top' => -10,
+                ],
+            ],
             'plugins' => [
                 'legend' => [
-                    'display' => true
+                    'title' => [
+                        'display' => true,
+
+                    ],
                 ],
                 'datalabels' => [
                     'color' => '#000000',
