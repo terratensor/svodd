71e1cf369d186e2caeda58b986b15ed1d4ac5182
agusev <audetv@gmail.com>
2023-04-02 22:15:21 +0300

    изменен репозиторий диаграммы

--- app/src/Svodd/Entity/Chart/SvoddChartRepository.php
+++ app/src/Svodd/Entity/Chart/SvoddChartRepository.php
@@ -4,10 +4,28 @@ declare(strict_types=1);
 
 namespace App\Svodd\Entity\Chart;
 
+use yii\db\ActiveRecord;
+
 class SvoddChartRepository
 {
     public function findAll(): array
     {
         return Data::find()->all();
+    }
+
+    public function save(Data $data): void
+    {
+        if (!$data->save()) {
+            throw new \RuntimeException('Saving error.');
+        }
+    }
+
+    /**
+     * @param int $question_id
+     * @return array|ActiveRecord|Data|null
+     */
+    public function findByQuestionId(int $question_id): array|ActiveRecord|null|Data
+    {
+        return Data::find()->andWhere(['question_id' => $question_id])->one();
     }
 }
