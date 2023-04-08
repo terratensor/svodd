a00b8503e33b352118c93e29a01cdf11282b510c
agusev <audetv@gmail.com>
2023-04-09 00:33:25 +0300

    Добавлена ссылка на вопрос

--- app/frontend/views/question/question.php
+++ app/frontend/views/question/question.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 
 use App\Question\Entity\Question\Question;
 use frontend\widgets\question\QuestionHeader;
+use yii\helpers\Html;
 
 ?>
 <div class="collapse" id="collapseQuestion">
@@ -17,6 +18,14 @@ use frontend\widgets\question\QuestionHeader;
       <div class="card-text comment-text">
           <?php echo Yii::$app->formatter->asRaw($question->text); ?>
       </div>
+    </div>
+    <div class="card-footer d-flex justify-content-end">
+        <?php $link = "https://фкт-алтай.рф/qa/question/view-" . $question->data_id; ?>
+        <?= Html::a(
+            $link,
+            $link . "#:~:text=" . $question->datetime->format('H:i d.m.Y'),
+            ['target' => '_blank', 'rel' => 'noopener noreferrer']
+        ); ?>
     </div>
   </div>
 </div>
