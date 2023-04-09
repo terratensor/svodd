3d5707ec82d857d39980997867f7ece431769038
agusev <audetv@gmail.com>
2023-04-09 22:58:00 +0300

    Добавлена яндекс метрика на рабочий сервер

--- app/frontend/views/layouts/layout.php
+++ app/frontend/views/layouts/layout.php
@@ -11,25 +11,26 @@ use yii\web\View;
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-    <!DOCTYPE html>
-    <html lang="<?= Yii::$app->language ?>" class="h-100" data-bs-theme="svodd">
-    <head>
-        <meta charset="<?= Yii::$app->charset ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <?php $this->registerCsrfMetaTags() ?>
-        <title><?= Html::encode($this->title) ?></title>
-        <?php $this->head() ?>
-    </head>
-    <body class="d-flex flex-column h-100">
-    <?php $this->beginBody() ?>
+  <!DOCTYPE html>
+  <html lang="<?= Yii::$app->language ?>" class="h-100" data-bs-theme="svodd">
+  <head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+      <?php $this->head() ?>
+      <?= $this->render('yandex_metrika'); ?>
+  </head>
+  <body class="d-flex flex-column h-100">
+  <?php $this->beginBody() ?>
 
-    <?= $this->render('header'); ?>
+  <?= $this->render('header'); ?>
 
-    <?= $content ?>
+  <?= $content ?>
 
-    <?= $this->render('footer'); ?>
+  <?= $this->render('footer'); ?>
 
-    <?php $this->endBody() ?>
-    </body>
-    </html>
+  <?php $this->endBody() ?>
+  </body>
+  </html>
 <?php $this->endPage();
--- app/frontend/views/layouts/search.php
+++ app/frontend/views/layouts/search.php
@@ -22,6 +22,7 @@ AppAsset::register($this);
       <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
       <?php $this->head() ?>
+      <?= $this->render('yandex_metrika'); ?>
   </head>
   <body class="d-flex flex-column h-100">
   <?php $this->beginBody() ?>
