7036be60248077235d4836bfd4f928d3f2a3ebbd
agusev <audetv@gmail.com>
2023-04-13 22:51:04 +0300

    Добавлены обработчик для чтения файлов и отправки в TopicRenewService для обновления записей в БД

---
+++ app/src/Indexer/Service/UpdateDbFromParsedFiles/Handler.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Indexer\Service\UpdateDbFromParsedFiles;
+
+use App\Indexer\Service\DirectoryService;
+use App\Indexer\Service\Files\RemoveFileService;
+use App\Indexer\Service\JsonUnmarshalService;
+use App\Indexer\Service\ReadFileService;
+use App\Indexer\Service\TopicRenewService;
+use App\Question\Entity\Question\QuestionRepository;
+
+/**
+ * Этот обработчик обновляет только уже существующие в БД записи в таблицах question и question_comments
+ * Сервис читает файлы, сохраненные парсером, если запись в БД существует, то обновляет эту запись данными из файла,
+ * если запись не существует, то пропускает. Пропущенная запись позже будет обработана командой index/updating-index
+ */
+class Handler
+{
+    private DirectoryService $directoryService;
+    private ReadFileService $readFileService;
+    private JsonUnmarshalService $unmarshalService;
+    private RemoveFileService $removeFileService;
+    private QuestionRepository $questionRepository;
+    private TopicRenewService $topicRenewService;
+
+    public function __construct(
+        DirectoryService $directoryService,
+        ReadFileService $readFileService,
+        JsonUnmarshalService $unmarshalService,
+        QuestionRepository $questionRepository,
+        RemoveFileService $removeFileService,
+        TopicRenewService $topicRenewService
+    ) {
+        $this->directoryService = $directoryService;
+        $this->readFileService = $readFileService;
+        $this->unmarshalService = $unmarshalService;
+        $this->removeFileService = $removeFileService;
+        $this->questionRepository = $questionRepository;
+        $this->topicRenewService = $topicRenewService;
+    }
+
+    public function handle(): void
+    {
+        $files = $this->directoryService->readDir();
+
+        foreach ($files as $file) {
+            if ($doc = $this->readFileService->readFile($file)) {
+                echo "parsed: " . $file . "\n";
+                $this->updateQuestion($doc);
+                if ($this->removeFileService->handle($file)) {
+                    echo "successfully processed and deleted: " . $file . "\n";
+                }
+            }
+        }
+    }
+
+    /**
+     * @param string $doc
+     * @return void
+     * Преобразовываем тему в json, проверяем есть ли уже такой вопрос в БД, если нет, то создаем новые записи в БД и поисковом индексе, в противном случае обновляем индекс и БД новыми комментариями, обновляем таблицу статистики
+     */
+    private function updateQuestion(string $doc): void
+    {
+        $topic = $this->unmarshalService->handle($doc);
+        try {
+            $this->questionRepository->getByDataId($topic->question->data_id);
+            // Обновляем вопрос и все комментарии вопроса
+            $this->topicRenewService->renew($topic);
+        } catch (\Throwable $e) {
+            echo "The question is skipped: ", $e->getMessage(). "\n";
+        }
+    }
+}
