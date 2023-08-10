4171f81b869a19477d1f2b2ee6fcbf6d6ae4818b
audetv <algsound@yandex.ru>
2023-08-10 03:54:05 +0300

    Working dev version message sender, need filter for svodd

--- app/src/Question/Entity/listeners/CommentCreatedListener.php
+++ app/src/Question/Entity/listeners/CommentCreatedListener.php
@@ -5,12 +5,30 @@ declare(strict_types=1);
 namespace App\Question\Entity\listeners;
 
 use App\Question\Entity\Question\events\CommentCreated;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Exchange\AMQPExchangeType;
+use PhpAmqpLib\Message\AMQPMessage;
 
 class CommentCreatedListener
 {
     public function handle(CommentCreated $event): void
     {
-        $text = $event->text;
-        echo $text;
+        $exchange = 'ex1';
+        $queue = 'q1';
+
+        $connection = new AMQPStreamConnection('rmq', 5672, 'guest', 'guest', '/');
+        $channel = $connection->channel();
+//        $channel = $connection->channel();
+//
+        $channel->queue_declare($queue, false, true, false, false);
+        $channel->exchange_declare($exchange, AMQPExchangeType::DIRECT, false, true, false);
+
+        $channel->queue_bind($queue, $exchange);
+
+        $message = new AMQPMessage($event->text, array('content_type' => 'text/plain', 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
+        $channel->basic_publish($message, $exchange);
+
+        $channel->close();
+        $connection->close();
     }
 }
