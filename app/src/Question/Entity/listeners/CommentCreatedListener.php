edc877c00f2ed06dc14d3557fa70509366c7c493
audetv <algsound@yandex.ru>
2023-08-10 14:56:25 +0300

    Changed comment created listener, added envs, condition for current question

--- app/src/Question/Entity/listeners/CommentCreatedListener.php
+++ app/src/Question/Entity/listeners/CommentCreatedListener.php
@@ -5,21 +5,42 @@ declare(strict_types=1);
 namespace App\Question\Entity\listeners;
 
 use App\Question\Entity\Question\events\CommentCreated;
+use App\Svodd\Entity\Chart\SvoddChartRepository;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Exchange\AMQPExchangeType;
 use PhpAmqpLib\Message\AMQPMessage;
 
 class CommentCreatedListener
 {
+    private SvoddChartRepository $repository;
+
+    public function __construct(SvoddChartRepository $repository) {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @throws \Exception
+     */
     public function handle(CommentCreated $event): void
     {
-        $exchange = 'ex1';
-        $queue = 'q1';
+        $current = $this->repository->findCurrent();
 
-        $connection = new AMQPStreamConnection('rmq', 5672, 'guest', 'guest', '/');
+        if ($current->question_id !== $event->question_data_id) {
+            return;
+        }
+
+        $exchange = getenv('RABBIT_EXCHANGE_NAME');
+        $queue = getenv('RABBIT_QUEUE_NAME');
+
+        $connection = new AMQPStreamConnection(
+            getenv('RABBIT_HOSTNAME'),
+            5672,
+            getenv('RABBIT_USERNAME'),
+            getenv('RABBIT_PASSWORD'),
+            '/'
+        );
         $channel = $connection->channel();
-//        $channel = $connection->channel();
-//
+
         $channel->queue_declare($queue, false, true, false, false);
         $channel->exchange_declare($exchange, AMQPExchangeType::DIRECT, false, true, false);
 
