2ebb3b4c63ce52ea9e34ddd22d2f8f122daebd1f
audetv <algsound@yandex.ru>
2023-08-11 23:46:17 +0300

    Changed site footer

--- app/frontend/views/layouts/footer.php
+++ app/frontend/views/layouts/footer.php
@@ -6,8 +6,7 @@ use yii\bootstrap5\Html;
 <footer class="footer mt-auto py-3 text-muted">
   <div class="container-fluid">
     <div class="d-flex align-items-baseline justify-content-between">
-      <span><?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?> <?= \App\helpers\VersionHelper::version(); ?>
-          <?= Html::a('Обратная связь', ['site/contact']); ?>
+      <span><?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?>
           <?php
           if (Yii::$app->user->isGuest) {
               echo Html::a('Вход', ['/auth/auth/login'], ['class' => ['']]);
@@ -16,19 +15,7 @@ use yii\bootstrap5\Html;
           }
           ?>
       </span>
-      <span><?= Html::tag(
-              'a',
-              '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-</svg>',
-              [
-                  'class ' => 'btn btn-link link-dark',
-                  'aria-label' => "svodd",
-                  'href' => 'https://github.com/terratensor/svodd',
-                  'target' => "_blank",
-                  'rel' => "noopener noreferrer",
-
-              ]); ?></span>
+      <span><?= Html::a('Обратная связь', ['site/contact']); ?></span>
     </div>
   </div>
 </footer>
