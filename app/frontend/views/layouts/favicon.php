d546af1c91286ca982b5d8b083cdcb9b8c86d348
audetv <algsound@yandex.ru>
2023-08-12 00:11:16 +0300

    Added to favicons absolute urls

--- app/frontend/views/layouts/favicon.php
+++ app/frontend/views/layouts/favicon.php
@@ -1,30 +1,39 @@
 <?php $this->registerLinkTag(
     [
         'rel' => 'icon', 'type' => 'image/png', 'sizes' => '16x16',
-        'href' => '/favicon/favicon-16x16.png'
+        'href' => Yii::$app->urlManager->createAbsoluteUrl('/favicon/favicon-16x16.png')
     ]
 ) ?>
 <?php $this->registerLinkTag(
     [
         'rel' => 'icon', 'type' => 'image/png', 'sizes' => '32x32',
-        'href' => '/favicon/favicon-32x32.png'
+        'href' => Yii::$app->urlManager->createAbsoluteUrl('/favicon/favicon-32x32.png')
     ]
 ) ?>
 <?php $this->registerLinkTag(
     [
         'rel' => 'icon', 'type' => 'image/png', 'sizes' => '192x192',
-        'href' => '/favicon/android-chrome-192x192.png'
+        'href' => Yii::$app->urlManager->createAbsoluteUrl('/favicon/android-chrome-192x192.png')
     ]
 ) ?>
-<?php $this->registerLinkTag([
-                                 'rel' => 'icon', 'type' => 'image/png', 'sizes' => '512x512',
-                                 'href' => '/favicon/android-chrome-512x512.png'
-                             ]) ?>
+<?php $this->registerLinkTag(
+    [
+        'rel' => 'icon', 'type' => 'image/png', 'sizes' => '512x512',
+        'href' => Yii::$app->urlManager->createAbsoluteUrl('/favicon/android-chrome-512x512.png')
+    ]) ?>
 <?php $this->registerLinkTag(
     [
         'rel' => 'apple-touch-icon', 'sizes' => '180x180',
-        'href' => '/favicon/apple-touch-icon.png'
+        'href' => Yii::$app->urlManager->createAbsoluteUrl('/favicon/apple-touch-icon.png')
     ]
 ) ?>
-<?php $this->registerLinkTag(['rel' => 'shortcut icon', 'href' => '/favicon.ico']) ?>
-<?php $this->registerLinkTag(['rel' => 'manifest', 'href' => '/favicon/site.webmanifest']) ?>
+<?php $this->registerLinkTag(
+    [
+        'rel' => 'shortcut icon',
+        'href' => Yii::$app->urlManager->createAbsoluteUrl('/favicon.ico')
+    ]) ?>
+<?php $this->registerLinkTag(
+    [
+        'rel' => 'manifest'
+        , 'href' => Yii::$app->urlManager->createAbsoluteUrl('/favicon/site.webmanifest')
+    ]) ?>
