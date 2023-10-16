3d3c598f79986552023fd3eb113ee2d8f28b2839
agusev <audetv@gmail.com>
2023-10-16 20:59:10 +0300

    Fixed #258, переписана функция, используется DOMDocument

--- app/src/helpers/TgLinkClipper.php
+++ app/src/helpers/TgLinkClipper.php
@@ -4,31 +4,52 @@ declare(strict_types=1);
 
 namespace App\helpers;
 
+use DOMAttr;
+use DOMDocument;
+use DOMNode;
+
 class TgLinkClipper
 {
     public static function process(string $text): string
     {
         if (\Yii::$app->params['cut_telegram_links']) {
-            return preg_replace_callback(
-                '|(<span class="link">)(.+)(</span>)|iu', function ($matches) {
 
-                if (preg_match("/https:\/\/t.me\/svoddru/", $matches[2], $mm)) {
-                    return $matches[1] . $matches[2] . $matches[3];
-                }
+            $doc = new DOMDocument('1.0', 'UTF-8');
+            // With a DOMDocument object, you should be able to place an @ before the load method in order to SUPPRESS all WARNINGS.
+            @$doc->loadHTML(mb_convert_encoding($text, 'HTML-ENTITIES', 'UTF-8'));
 
-                if (preg_match("/https:\/\/t.me/", $matches[2], $mm)) {
-                    return preg_replace("/https:\/\//", "", $matches[2]);//
-                }
+            $nodes = $doc->getElementsByTagName('span');
 
-                if (preg_match("/http:\/\/t.me/", $matches[2], $mm)) {
-                    return preg_replace("/http:\/\//", "", $matches[2]);//
-                }
+            /** @var DOMNode $node */
+            foreach ($nodes as $node) {
+                self::parseNode($node);
+            }
 
-                return $matches[1] . $matches[2] . $matches[3];
-
-            },  $text);
+            return $doc->saveHTML();
         }
 
         return $text;
+    }
+
+    private static function parseNode(DOMNode $node): void
+    {
+        /** @var DOMAttr $attribute */
+        foreach ($node->attributes as $attribute) {
+            $value = $node->nodeValue;
+            if ($attribute->name == 'class' && $attribute->value === 'link') {
+                var_dump("true");
+                if (preg_match("/https:\/\/t.me\/svoddru/", $value, $mm)) {
+                    break;
+                }
+                if (preg_match("/https:\/\/t.me/", $value, $mm)) {
+                    $attribute->value = 'no-link';
+                    break;
+                }
+                if (preg_match("/http:\/\/t.me/", $value, $mm)) {
+                    $attribute->value = 'no-link';
+                    break;
+                }
+            }
+        }
     }
 }
