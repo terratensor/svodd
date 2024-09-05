2462ccddf11921f6d22fe4b8a7f5319e41d0bb60
audetv <algsound@yandex.ru>
2024-09-05 22:08:19 +0300

    Изменен action короткой ссылки, добавлено сохранение поискового результата

--- app/src/UrlShortener/Http/Action/V1/UrlShortener/ShortLinkAction.php
+++ app/src/UrlShortener/Http/Action/V1/UrlShortener/ShortLinkAction.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\UrlShortener\Http\Action\V1\UrlShortener;
 
+use App\SearchResults\Command\Create\Command as CreateCommand;
+use App\SearchResults\Command\Create\Handler as SearchResultHandler;
 use App\UrlShortener\Command\Create\Request\Command;
 use App\UrlShortener\Command\Create\Request\Handler;
 use App\UrlShortener\Form\CreateLink\CreateForm;
@@ -15,11 +17,18 @@ use yii\httpclient\Exception;
 class ShortLinkAction extends Action
 {
     private Handler $handler;
+    private SearchResultHandler $searchResultHandler;
 
-    public function __construct($id, $controller, Handler $handler, $config = [])
-    {
+    public function __construct(
+        $id,
+        $controller,
+        Handler $handler,
+        SearchResultHandler $searchResultHandler,
+        $config = []
+    ) {
         parent::__construct($id, $controller, $config);
         $this->handler = $handler;
+        $this->searchResultHandler = $searchResultHandler;
     }
 
     /**
@@ -28,19 +37,31 @@ class ShortLinkAction extends Action
      */
     public function run(): array|string|\yii\httpclient\Response
     {
-        $form = new CreateForm();
+        $createForm = new CreateForm();
 
-        try {
-            if ($form->load(Yii::$app->request->post()) && $form->validate()) {
-                $command = new Command();
-                $command->origin = $form->origin;
-                return $this->handler->handle($command);
+        if ($createForm->load(Yii::$app->request->post()) && $createForm->validate()) {
+            $command = new Command();
+            $command->origin = $createForm->origin;
+
+            try {
+                $response = $this->handler->handle($command);
+
+                if ($user = Yii::$app->user->getIdentity()) {
+                    $searchResultCommand = new CreateCommand();
+                    $searchResultCommand->user_id = $user->getId();
+                    $searchResultCommand->short_link = json_decode($response)->short ?? '';
+                    if ($searchResultCommand->short_link) {
+                        $this->searchResultHandler->handle($searchResultCommand);
+                    }
+                }
+
+                return $response;
+            } catch (\Throwable $e) {
+                Yii::$app->errorHandler->logException($e);
             }
-        } catch (\DomainException $e) {
-            Yii::$app->errorHandler->logException($e);
         }
 
         Yii::$app->response->statusCode = 400;
-        return json_encode(["message" => "Bad Request"], JSON_THROW_ON_ERROR);
+        return json_encode(['message' => 'Bad Request'], JSON_THROW_ON_ERROR);
     }
 }
